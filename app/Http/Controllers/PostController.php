318a5bd4ebb3e4acdf58f9f82375b043823a1fd2
Linh <22520780@gm.uit.edu.vn>
2025-09-15 09:40:52 +0700

    add Filtering posts by category

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'DESC')->paginate(10);
+        $posts = Post::latest()->paginate(10);
         
         return view('post.index', [ 
             'posts'=>$posts
@@ -92,4 +92,14 @@ class PostController extends Controller
     {
         //
     }
+
+    public function category(Category $category)
+    {
+        $posts = $category->posts()->latest()->simplePaginate(5);
+
+        return view('post.index', [
+            'posts' => $posts,
+        ]);
+    }
 }
+
